d9e88ffa36aa05be882d45c0cbf312aad7cb3c76
Brian at Podping <brian@podping.org>
2022-09-04 09:54:03 +0300

    remove ipfs.io
    Signed-off-by: Brian at Podping <brian@podping.org>

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const snapcs = ENV.SNAPBASE_URL || "http://65.108.66.120:8002"; // get a public 
 const history = ENV.history || 3600
 const stream = ENV.stream || 'irreversible'
 const mode = ENV.mode || 'normal'
-const timeoutStart = ENV.timeoutStart || 180000 
+const timeoutStart = ENV.timeoutStart || 180000
 const timeoutContinuous = ENV.timeoutContinuous || 30000
 
 
@@ -36,7 +36,7 @@ const rtp = ENV.rtp || '' //rtrades password : IPFS pinning interface
 const ipfshost = ENV.ipfshost || 'ipfs.infura.io' //IPFS upload/download provider provider
 const ipfsport = ENV.ipfsport || '5001' //IPFS upload/download provider provider
 
-const ipfsLinks = ENV.ipfsLinks ? ENV.ipfsLinks.split(' ') : [ "https://ipfs:8080/ipfs/", "http://localhost:8080/ipfs/", "https://ipfs.io/ipfs/","https://ipfs.infura.io/ipfs/",]
+const ipfsLinks = ENV.ipfsLinks ? ENV.ipfsLinks.split(' ') : [ "https://ipfs:8080/ipfs/", "http://localhost:8080/ipfs/", "https://ipfs.infura.io/ipfs/",]
 
 const ipfsprotocol = ENV.ipfsprotocol || 'https' //IPFS upload/download protocol
 //node market config > 2500 is 25% inflation to node operators, this is currently not used
@@ -62,7 +62,7 @@ const TOKEN = 'LARYNX' //Token name
 const precision = 3 //precision of token
 const tag = 'spk' //the fe.com/<tag>/@<leader>/<permlink>
 const jsonTokenName = 'larynx' //what customJSON in Escrows and sends is looking for
-const leader = 'regardspk' //Default account to pull state from, will post token 
+const leader = 'regardspk' //Default account to pull state from, will post token
 const ben = '' //Account where comment benifits trigger token action
 const delegation = '' //account people can delegate to for rewards
 const delegationWeight = 1000 //when to trigger community rewards with bens
