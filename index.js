f23c57c5deb953f5c9948ea5cdd8577960d8c3a8
disregardfiat <sjettingerjr@gmail.com>
2024-01-31 17:41:54 +0100

    api update

--- index.js
+++ index.js
@@ -1035,7 +1035,6 @@ function startWith(hash, second) {
                 if (!e && (second || data[0] > API.RAM.head - 325)) {
                   if (hash) {
                     var cleanState = data[1];
-                    delete cleanState.spkVote //remove after next 
                     if (config.mirrorNet && hash == replay) { //test net and upgrade init
                       delete cleanState.powd
                       delete cleanState.govd
