a0482d91748e965872a0e6188aa8e7b4b2afb1e4
disregardfiat <sjettingerjr@gmail.com>
2025-03-15 18:17:49 -0300

    ++

--- index.js
+++ index.js
@@ -1062,6 +1062,8 @@ function startWith(hash, second) {
                 if (!e && (second || data[0] > API.RAM.head - 325)) {
                   if (hash) {
                     var cleanState = data[1];
+                    cleanState.lbroca = cleanState.spk
+                      cleanState.bpow = cleanState.spow
                     if (config.mirrorNet && hash == replay) { //test net and upgrade init
                       // delete cleanState.powd
                       // delete cleanState.govd
