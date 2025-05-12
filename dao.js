99677a7aec0fdd3abc7b92ae90cf91ce7bb92fe5
disregardfiat <sjettingerjr@gmail.com>
2025-05-12 19:51:16 -0300

    1.5

--- dao.js
+++ dao.js
@@ -66,6 +66,7 @@ export function dao(num) {
                 paidCleaner = v[13],
                 rnftsCleaner = v[14],
                 dist = v[15]
+            console.log({stats})
             for(var i = 0; i < dist.length;i++){
                 if(dist[i][0].split('div:')[1]){
                     addMT(['div', dist[i][0].split('div:')[1], 'b'], dist[i][1] )
@@ -478,6 +479,8 @@ export function dao(num) {
               });
             }
             console.log({stats})
+            //{ daoRate: 2500, movingWeight: { dailyPool: 66259896 } }
+
             daops.push({ type: 'put', path: ['dex'], data: dex });
             daops.push({ type: 'put', path: ['stats'], data: stats });
             daops.push({ type: 'put', path: ['balances'], data: bals });
