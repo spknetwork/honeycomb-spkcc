1601ac0057aed48e142e34eb22ff2c1e06d05814
disregardfiat <sjettingerjr@gmail.com>
2025-05-20 20:31:36 -0300

    ++

--- index.mjs
+++ index.mjs
@@ -986,6 +986,7 @@ Promise.all([config.startURL, config.clientURL]).then(urls => {
                   if (!e && (second || data[0] > API.RAM.head - 325)) {
                     if (hash) {
                       var cleanState = data[1];
+                      if(!dyn)delete cleanState.chain //testing things
                       if (cleanState.stats.chain){
                         cleanState.chain = cleanState.stats.chain
                         delete cleanState.stats.chain
@@ -1020,7 +1021,6 @@ Promise.all([config.startURL, config.clientURL]).then(urls => {
                         CustomOperationsProcessing: config.CustomOperationsProcessing,
                         CustomChron: config.CustomChron
                       }
-                      //if(!dyn)delete cleanState.chain //testing things
 
                       store.put([], cleanState, function (err) {
                         if (err) {
