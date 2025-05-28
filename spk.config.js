7783526714559b9c31ec11c6cc62a792d87549f5
disregardfiat <sjettingerjr@gmail.com>
2025-05-28 19:02:11 -0300

    2025.05.28

--- spk.config.js
+++ spk.config.js
@@ -369,8 +369,8 @@ const CodeShare = {
         else total += val[n[i]] || 1
       }
       const gte = CodeShare.PoA.getPrand58(account, prand, context)
-      const range = parseInt(((val[account] >= cutoff ? cutoff * 2 : val[account] || 1) / total) * (stats.total_files * parseInt(stats.vals_target) / 28800) * 7427658739)
-      if(config.mode == 'verbose') console.log(range, stats.total_files, stats.vals_target)
+      const range = parseInt(((val[account] >= cutoff ? cutoff * 2 : val[account] || 1) / total) * (stats.total_files * parseInt(stats.vals_target) * 100 / 288) * 7427658739)
+      
       var lte = Base58.fromNumber(Base58.toNumber(gte) + range)
       if (lte.length > 9) lte = 'zzzzzzzzz'
       if (gte.length != lte.length) {
@@ -382,6 +382,7 @@ const CodeShare = {
           Base58.toNumber(gte)
         )
       }
+      if (config.mode == 'verbose') console.log('getRange', { gte, lte })
       return [gte, lte]
     },
     getPrand58(account, prand, context) {
