2bb93f40fb4c825bc3b05c1ad0b725b1eff4237d
disregardfiat <sjettingerjr@gmail.com>
2025-05-30 16:39:43 -0300

    2025.05.30

--- spk.config.js
+++ spk.config.js
@@ -8057,7 +8057,7 @@ const CustomChron = [
                 broca = broca_calc(mem[2], mem[3], stats, num),
                 renew = contract.m ? (contract.m.indexOf('"') >= 0 ? Base64.toNumber(JSON.parse(contract.m)[0]) & 1 : Base64.toNumber(contract.m[0]) & 1) : 0
               if (contract.c == 3 && renew && parseInt(broca.split(',')[0]) > 100) {
-                processor.doOp( 'extend', {
+                processor.doOn( 'extend', {
                   broca: parseInt(broca.split(',')[0]) > parseInt(3 * contract.r / contract.p) ? parseInt(3 * contract.r / contract.p) + 1 : parseInt(parseInt(broca.split(',')[0]) / 2) + 1,
                   id: contract.i,
                   file_owner: contract.t,
