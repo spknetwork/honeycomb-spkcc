0d7005edd7b90a469bd6d961619fe6cc532a61c7
disregardfiat <sjettingerjr@gmail.com>
2025-07-08 16:04:18 -0300

    2025.07.08

--- processing_routes/dex.js
+++ processing_routes/dex.js
@@ -2169,8 +2169,8 @@ export const feed_publish = (tx, pc, runtimeContext) => {
       if (witnessCount >= 4) {
         const base = naizer(tx.exchange_rate.base)
         const quote = naizer(tx.exchange_rate.quote)
-          console.log(base, quote)
-          const hivePerHbd = parseFloat((quote.amount / base.amount)).toFixed(3);
+        
+          const hivePerHbd = parseFloat((quote.amount.amount / base.amount.amount)).toFixed(3);
           
           // Store price feed with block number for staleness checking
           priceFeeds[publisher] = {
