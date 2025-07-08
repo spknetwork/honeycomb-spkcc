e01d487d776de9b37ed3605872d9c5526cda173a
disregardfiat <sjettingerjr@gmail.com>
2025-07-08 03:35:54 -0300

    2025.07.08

--- ipfsSaveState.js
+++ ipfsSaveState.js
@@ -4,6 +4,7 @@ import { of as createHash } from "ipfs-only-hash";
 export const ipfsHash = (num, buffer) => {
   return new Promise( async (resolve, reject) => {
     const hash = await createHash(buffer);
+    console.log(num + `:Hash: ${hash}`);
     plasma.hashLastIBlock = hash
     plasma.hashBlock = num
     store.batch([
